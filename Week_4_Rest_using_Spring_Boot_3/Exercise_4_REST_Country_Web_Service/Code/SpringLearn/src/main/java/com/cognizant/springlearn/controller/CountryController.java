8021ed8875f32addb3910f02a1cd37458095cd42
Vamsi1807 <vamsigopavarapu07@gmail.com>
2025-07-13 11:23:22 +0530

    exercise 4 competed

---
+++ Week_4_Rest_using_Spring_Boot_3/Exercise_4_REST_Country_Web_Service/Code/SpringLearn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,26 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CountryController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    static Country getCountryIndia(){
+
+        logger.info("inside getCountryIndia()");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = (Country) context.getBean("country");
+        logger.info("executed getCountryIndia()");
+        return country;
+    }
+}
