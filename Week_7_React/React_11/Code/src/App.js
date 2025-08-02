8fc2cd96b7ed0cb1e849172fd6444c64db772071
Vamsi1807 <vamsigopavarapu07@gmail.com>
2025-08-02 13:49:28 +0530

    10,11,12 completed

---
+++ Week_7_React/React_11/Code/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import EventButtons from './components/eventbuttons';
+import CurrencyConverter from './components/currencyconverter';
+
+function App() {
+  return (
+    <div>
+      <EventButtons/>
+      <CurrencyConverter/>
+    </div>
+  );
+}
+
+export default App;
